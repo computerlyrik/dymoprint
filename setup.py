981981a905dc412f6ef9ec093cd2c87015409cb5
Ben Mares <15216687+maresb@users.noreply.github.com>
2020-09-06 00:37:44 +0200

    Python 3.4 no longer supported due to Pillow version

--- setup.py
+++ setup.py
@@ -20,10 +20,10 @@ setup(
     author="Sebastian J. Bronner",
     author_email="waschtl@sbronner.com",
     license="Apache License 2.0",
-    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*",
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*",
     install_requires=[
         "appdirs",
-        "Pillow==6.2.1",
+        "Pillow==6.2.2",
         "PyQRCode==1.2.1",
         "pyBarcode==0.8b1; python_version=='2.7'",
         "python-barcode==0.9.0; python_version > '3.0'",
