2fefa5e9c4dc81b001bbbaf1f4625177dd8082b1
Ben Mares <15216687+maresb@users.noreply.github.com>
2020-10-04 23:55:40 +0200

    Fix typo

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     url="https://github.com/computerlyrik/dymoprint",
     author="Sebastian J. Bronner",
     author_email="waschtl@sbronner.com",
-    maintainter="Ben Mares",
+    maintainer="Ben Mares",
     maintainer_email="services-dymoprint@tensorial.com",
     license="Apache License 2.0",
     python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*",
