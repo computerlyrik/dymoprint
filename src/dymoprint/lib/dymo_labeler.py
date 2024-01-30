a61e8d1ba12cb16e5e3901917084835cdf65c4f7
Tomer Shalev <tshalev@proofpoint.com>
2024-01-30 20:50:41 +0000

    Coding: rename variable

--- src/dymoprint/lib/dymo_labeler.py
+++ src/dymoprint/lib/dymo_labeler.py
@@ -32,7 +32,7 @@ class DymoLabeler:
     <https://download.dymo.com/dymo/technical-data-sheets/LW%20450%20Series%20Technical%20Reference.pdf>
     """
 
-    DEFAULT_TAP_SIZE_MM = 12
+    DEFAULT_TAPE_SIZE_MM = 12
 
     tape_size_mm: int
 
@@ -56,7 +56,7 @@ class DymoLabeler:
     ):
         """Initialize the LabelManager object (HLF)."""
         if not tape_size_mm:
-            tape_size_mm = self.DEFAULT_TAP_SIZE_MM
+            tape_size_mm = self.DEFAULT_TAPE_SIZE_MM
         self._tape_size_mm = tape_size_mm
         self._cmd: list[int] = []
         self._response = False
@@ -70,7 +70,7 @@ class DymoLabeler:
     @classmethod
     def _max_bytes_per_line(cls, tape_size_mm: int | None = None) -> int:
         if not tape_size_mm:
-            tape_size_mm = cls.DEFAULT_TAP_SIZE_MM
+            tape_size_mm = cls.DEFAULT_TAPE_SIZE_MM
         return int(8 * tape_size_mm / 12)
 
     @classmethod
